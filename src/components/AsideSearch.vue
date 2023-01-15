fa8301a84e622838b9ae576c0229402e80242342
danielgolden <danielgolden90@gmail.com>
2023-01-15 09:16:21 -0500

    More minor style tweaks

--- src/components/AsideSearch.vue
+++ src/components/AsideSearch.vue
@@ -131,7 +131,7 @@ onMounted(() => {
   border: none;
   color: var(--color-text-primary);
   font-family: var(--font-family-primary);
-  transition: box-shadow 150ms var(--ease-out-cubic);
+  transition: box-shadow 200ms var(--ease-out-cubic);
 }
 
 @media (prefers-color-scheme: light) {
