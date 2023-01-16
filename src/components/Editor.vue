0138a1fdd9c09de9c96c98c84379af0fc9115cb4
danielgolden <danielgolden90@gmail.com>
2023-01-15 22:04:26 -0500

    Don't update on every keystroke of editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -31,18 +31,26 @@ const props = defineProps(["modelValue"]);
 const codemirrorContainer = ref<Element | null>(null);
 let myCodemirrorView = new EditorView();
 const codeMirrorTriggeredNoteCreation = ref(false);
+let onChangeTimer = ref(setTimeout(() => {}, 0));
 
 const handleOnChange = (update: ViewUpdate) => {
+  const waitTime = 500; // in milliseconds
+  clearTimeout(onChangeTimer.value);
+
   if (!update.docChanged) return;
   const currentContent = update.view.state.doc.toString();
 
   if (store.activeNoteId) {
     emit("update:modelValue", currentContent);
-    saveCurrentNoteChange(currentContent);
+    onChangeTimer.value = setTimeout(() => {
+      saveCurrentNoteChange(currentContent);
+    }, waitTime);
   } else {
     createNewNote();
     emit("update:modelValue", currentContent);
-    saveCurrentNoteChange(currentContent);
+    onChangeTimer.value = setTimeout(() => {
+      saveCurrentNoteChange(currentContent);
+    }, waitTime);
 
     codeMirrorTriggeredNoteCreation.value = true;
   }
